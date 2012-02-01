2a28b40ecac12d67d040cfc4b4bd79c4ca91db3e
Vladimir Dronnikov <dronnikov@gmail.com>
2012-02-01 12:12:43 -0500

    bundle cut version of curl

--- tests/package.lua
+++ tests/package.lua
@@ -6,5 +6,6 @@ return {
   dependencies = {
     ["meta-fs"] = "https://github.com/dvv/luvit-meta-fs/zipball/master",
     ["curl"] = "https://github.com/dvv/luvit-curl/zipball/master",
+    ["websocket"] = "https://github.com/dvv/luvit-websocket/zipball/master",
   },
 }
