7eb67c632f93221b5914aeeb28a4fafa28894b21
Vladimir Dronnikov <dronnikov@gmail.com>
2012-02-01 13:26:47 +0000

    wget

---
+++ tests/package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "balls",
+  version = "0.0.1",
+  description = "Utilities to deal with zipballs and tarballs",
+  author = "Vladimir Dronnikov <dronnikov@gmail.com>",
+  dependencies = {
+    ["meta-fs"] = "https://github.com/dvv/luvit-meta-fs/zipball/master",
+  },
+}
